0b6b62382e1aede4ce8661c1cdb6a4a39304ee8a
s.lopezmazo <s.lopezmazo@globant.com>
2023-09-05 11:41:22 -0500

    Cart page and click on checkout test

---
+++ src/test/java/com/globant/pages/CartPage.java
@@ -0,0 +1,30 @@
+package com.globant.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class CartPage extends BasePage {
+    @FindAll({
+            @FindBy(css = ".cart_item button[id^=remove]") // Replace with the appropriate locator
+    })
+    private List<WebElement> RemoveButtonsFromCartButtons;
+
+    @FindBy(id="checkout")
+    private WebElement checkoutButton;
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+    public void removeCartElements(){
+        for (WebElement removeButton:RemoveButtonsFromCartButtons){
+            clickElement(removeButton);
+        }
+        RemoveButtonsFromCartButtons.clear();
+    }
+    public void clickCheckout(){
+        clickElement(checkoutButton);
+    }
+}
